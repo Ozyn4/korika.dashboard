b9981347e0f4d7e4138787d34c54e2eb093f507c
oojn4 <fauzanfaldy4@gmail.com>
2024-07-26 11:05:20 +0700

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'peta.resiko.penyakit.dev',
   plugins: [react()],
   optimizeDeps: {
     exclude: ["parquet-wasm/esm/arrow2"],
